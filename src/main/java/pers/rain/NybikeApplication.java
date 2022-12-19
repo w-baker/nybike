80e8e6cb1ca0c7483e8ef405b88c5bfe8b141184
JackWang <1915358757@qq.com>
2022-12-19 14:41:31 +0800

    Initial commit

---
+++ src/main/java/pers/rain/NybikeApplication.java
@@ -0,0 +1,13 @@
+package pers.rain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NybikeApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(NybikeApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/pers/rain/NybikeApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.rain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NybikeApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
